5bdbd1c5493ba7acb01cdbe07758dba979753255
Lukas Praninskas <l.praninskas@gmail.com>
2019-04-27 00:39:50 +0200

    Change how dependencies are installed for Ubuntu

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -6,6 +6,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   brew install gtk+3 && brew link gtk+3
   brew install opencv && brew link opencv
 else
+  sudo apt-get install -y - libgmp-dev build-essential cmake git gtk2.0 gtk+3.0 libgtk2.0-dev libgtk-3-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev python-setuptools
   wget http://netix.dl.sourceforge.net/project/s3tools/s3cmd/2.0.2/s3cmd-2.0.2.tar.gz
   tar xvfz s3cmd-2.0.2.tar.gz
   cd s3cmd-2.0.2
